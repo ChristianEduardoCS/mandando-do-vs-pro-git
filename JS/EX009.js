71711fe3c5b44fdd4dc777c054e6c7a4ab45f203
christian <chriseduardocsalles@gmail.com>
2026-08-27 21:45:56 -0300

    abroba

--- JS/EX009.js
+++ JS/EX009.js
@@ -10,7 +10,7 @@ let soma = numero1 + numero2
 console.log(`${numero1} + ${numero2} = ${soma}`) 
 
 
-Function somarNumeros(n1,n2){ //SÓ É EXECUTADA A FUNÇÃO somarNumeros
+Function = somarNumeros(n1,n2){//SÓ É EXECUTADA A FUNÇÃO somarNumeros
 const soma = n1 + n2 
 console.log(`${n1} + ${n2}= ${soma}`)
 
